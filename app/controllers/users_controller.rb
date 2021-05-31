b0632e30d400e9fe4943f42270b3162b254f0e43
Barack <barackmukelenga100@gmail.com>
2021-06-01 00:45:11 +0200

    Add the edit page and update controller for updating the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,27 @@ class UsersController < ApplicationController
   def create 
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
-    
+
     if @user.save
       redirect_to new_user_path
     else
       render :new
+    end
+  end
+
+  def edit 
+    @user =  User.find(params[:id])
+  end
+
+  def update
+    @user =  User.find(params[:id])
+    @user.update(user_params)
+
+    if @user.save
+      flash.notice = "User '#{@user.username}' updated successfully!"
+      redirect_to edit_user_path
+    else
+      render :edit
     end
   end
 
